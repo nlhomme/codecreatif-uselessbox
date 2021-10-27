c7113eecc2f8c41ce9b59797388935a2d7606fc6
Nicolas Lhomme <9362475+nlhomme@users.noreply.github.com>
2021-10-27 20:46:35 +0200

    Blink.ino: add a standby mode to avoid an exit statement and code cleaning

--- Blink.ino
+++ Blink.ino
@@ -2,7 +2,6 @@
   Blink
 
   Turns an LED on for one second, then off for one second, repeatedly while the button is not pressed.
-
   The signal goes from pin 2 to GND, passing through a button
 
   This code is based on the article below:
@@ -13,7 +12,7 @@
 // Setting the button's pin and its default value
 const int buttonPin = 2;
 int buttonState = 0;
-
+boolean standbyMode = false;
 
 // SETUP
 void setup() {
@@ -33,22 +32,34 @@ void loop() {
   // Reading the state of the button's value
   buttonState = digitalRead(buttonPin);
   
-  // Displaying the result (for DEBUG)
+  // Displaying the result (for DEBUG purpose)
   //Serial.println(buttonState);
   
   // The "INPUT_PULLUP" parameter reverts the button's logic
   // So if button pressed, buttonState is LOW
   if (buttonState == LOW) {
-    // Exiting the program
-    Serial.println("The button has been pressed, exiting...");
+    // If the button's state is pressed, toggle standby mode
+    standbyMode = !standbyMode;
+    if (standbyMode == true) {
+      Serial.println("Standby mode has been enabled, stop doing the job");
+    } else {
+      Serial.println("Standby mode disabled, resuming the job...");
+    }
     delay(1000);
-    exit(0);
+  }
+  
+  if (standbyMode == true) {
+    // Put a job to do here
+    // As we are in standby mode, do nothing
   } else {
-    digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+    // Put a job to do here
+    // Turn the LED on for a second, then off for a second
+    digitalWrite(LED_BUILTIN, HIGH);
     Serial.println("LED IS ON...");
-    delay(1000);                       // wait for a second
-    digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+    delay(1000);
+    
+    digitalWrite(LED_BUILTIN, LOW);
     Serial.println("LED IS OFF...");
-    delay(1000);                       // wait for a second
+    delay(1000);
   }
 }
